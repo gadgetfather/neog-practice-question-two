de5e10509f7258ea760da6e9888fa40c613a6867
Akshay Kamble <kamblea635@gmail.com>
2021-09-28 14:58:34 +0530

    completed task 2

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ var input= document.querySelector("#input")
 var buttonPlus = document.querySelector("#button-plus")
 var buttonMinus = document.querySelector("#button-minus")
 var outputEl = document.querySelector("#output")
+
+
+buttonPlus.addEventListener("click",function addtwo(){
+    var inputData = input.value
+    style = window.getComputedStyle(outputEl, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    outputEl.style.fontSize =(currentSize + 2) + 'px';
+    outputEl.textContent= inputData
+
+})
+
+buttonMinus.addEventListener("click",function removetwo(){
+    var inputData = input.value
+    style = window.getComputedStyle(outputEl, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    outputEl.style.fontSize =(currentSize -2) + 'px';
+    outputEl.textContent= inputData
+})
